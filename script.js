2c8379da95daf180234507dd1c93ebcbe68bdbef
SAMUEL DOS SANTOS CARVALHO <samuel_carvalho151@estudante.sesisenai.org.br>
2026-08-06 15:00:14 -0300

    Adiciona alteração deixando o botao azul

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ document.getElementById("btn1").addEventListener("click", function () {
 // Ao clicar no botao, mude a cor do texto #texto2 para azul
 //
 // Dica: use .style.color = "blue"
+document.getElementById("btn2").addEventListener("click", function(){
+    document.getElementById("texto2").style.color = "blue"
+
+
+} )
 
 
 
